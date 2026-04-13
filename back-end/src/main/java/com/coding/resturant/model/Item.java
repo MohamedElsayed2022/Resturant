6c0e25ea6c437556a54fe3ccf73681954de92370
MohamedElsayed2022 <mohamedsayed33333333@gmail.com>
2026-04-13 18:42:43 +0200

    Create Entity (RequestOrder _ Item) #35

---
+++ back-end/src/main/java/com/coding/resturant/model/Item.java
@@ -0,0 +1,8 @@
+package com.coding.resturant.model;
+
+public class Item {
+    private Long id;
+    private String img;
+    private int quantity;
+    private int price;
+}
---
+++ back-end/src/main/java/com/coding/resturant/model/RequestOrder.java
@@ -0,0 +1,10 @@
+package com.coding.resturant.model;
+
+public class RequestOrder {
+    private Long id;
+    private String code;
+    private String note ;
+    private int totalPrice;
+    private int totalQuantity;
+
+}
