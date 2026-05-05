93185360776f4a70b0b82dd2147af4a3408a110b
MohamedElsayed2022 <mohamedsayed33333333@gmail.com>
2026-05-05 15:34:06 +0300

    edit in restaurant name in main #48

--- back-end/src/main/java/com/coding/resturant/RestaurantApplication.java
+++ back-end/src/main/java/com/coding/resturant/RestaurantApplication.java
@@ -2,15 +2,15 @@ package com.coding.resturant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
 //import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
 //@EntityScan("com/coding/resturant/model")
-public class ResturantApplication {
+public class RestaurantApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ResturantApplication.class, args);
+        SpringApplication.run(RestaurantApplication.class, args);
     }
 
 }
