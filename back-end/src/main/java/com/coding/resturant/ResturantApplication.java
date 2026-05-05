4e22576cce9a5c13c6054d2aab0545ffce91b69f
MohamedElsayed2022 <mohamedsayed33333333@gmail.com>
2026-05-05 15:32:27 +0300

    add spring security dependency and updating in pom.xml #48

--- back-end/src/main/java/com/coding/resturant/ResturantApplication.java
+++ back-end/src/main/java/com/coding/resturant/ResturantApplication.java
@@ -2,10 +2,11 @@ package com.coding.resturant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com/coding/resturant/model")
+//@EntityScan("com/coding/resturant/model")
 public class ResturantApplication {
 
     public static void main(String[] args) {
